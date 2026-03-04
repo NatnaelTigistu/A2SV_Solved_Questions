2e5d1e498f2aba652c2aa80cc4fe85fba8c36e0f
Natnael Tigistu Gashaw <habtethiop@gmail.com>
2026-03-04 18:03:53 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -3,25 +3,22 @@ from typing import List
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         n = len(s)
-        diff = [0] * (n + 1)
+        shift = [0] * (n+1)
 
-        for l, r, d in shifts:
-            if d == 1:   # forward shift
-                diff[l] += 1
-                if r + 1 < n:
-                    diff[r + 1] -= 1
-            else:        # backward shift
-                diff[l] -= 1
-                if r + 1 < n:
-                    diff[r + 1] += 1
-
-        result = []
-        shift = 0
-
-        # Apply prefix sum and shift characters
+        for l,r,d in shifts:
+            if d == 0:
+                shift[l] -= 1
+                shift[r+1] += 1
+            else:
+                shift[l] += 1
+                shift[r+1] -= 1
+        _sum = 0
         for i in range(n):
-            shift += diff[i]
-            new_char = (ord(s[i]) - ord('a') + shift) % 26
-            result.append(chr(new_char + ord('a')))
-
-        return "".join(result)
+            _sum += shift[i]
+            shift[i] = _sum
+        res = []
+        _a = ord('a')
+        for i in range(n):
+            char = (ord(s[i]) - _a + shift[i]) % 26
+            res.append(chr(char + _a))
+        return ''.join(res)
