f9f214584168a853026b62fa81339916f2977ff3
Natnael Tigistu Gashaw <habtethiop@gmail.com>
2026-03-13 17:36:17 +0300

    Add solution for Jump Game

---
+++ leetcode/0055-jump-game/0055-jump-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if not nums or len(nums) == 1:
+            return True
+        nums = reversed(nums)
+        nums = list(nums)[1:]
+        r_j = 1
+        for num in nums:
+            if num >= r_j:
+                r_j = 1
+            else:
+                r_j += 1
+        if r_j <= nums[-1]:
+            return True
+        return False
