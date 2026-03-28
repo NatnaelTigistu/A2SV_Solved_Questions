9fa9426abfd555f8030e4d801e30211d1014d03b
Natnael Tigistu Gashaw <habtethiop@gmail.com>
2026-03-28 21:44:08 +0300

    Add solution for The k-th Lexicographical String of All Happy Strings of Length n

---
+++ leetcode/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -0,0 +1,39 @@
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        res = []
+        def backtrack(e):
+            if len(res) == k:
+                return
+            print(e)
+            if len(e) == n:
+                #append and return
+                res.append(''.join(c for c in e))
+                return 
+
+            if e[-1] == 'a':
+                e.append('b')
+                backtrack(e)
+                e.pop()
+                e.append('c')
+                backtrack(e)
+                e.pop()
+            elif e[-1] == 'b':
+                e.append('a')
+                backtrack(e)
+                e.pop()
+                e.append('c')
+                backtrack(e)
+                e.pop()
+            else:
+                e.append('a')
+                backtrack(e)
+                e.pop()
+                e.append('b')
+                backtrack(e)
+                e.pop()
+        for char in ['a','b','c']:
+            backtrack([char])
+    
+        if len(res) == k:
+            return res[k-1]
+        return ""
