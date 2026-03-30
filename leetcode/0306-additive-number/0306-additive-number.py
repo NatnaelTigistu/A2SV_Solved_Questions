14cfaa8f6248a691da2318abb7fd64079b0d501a
Natnael Tigistu Gashaw <habtethiop@gmail.com>
2026-03-30 18:37:09 +0300

    Add solution for Additive Number

--- leetcode/0306-additive-number/0306-additive-number.py
+++ leetcode/0306-additive-number/0306-additive-number.py
@@ -1,12 +1,16 @@
 class Solution:
     def isAdditiveNumber(self, num: str) -> bool:
+        #115358
+        greater = False
         valid = False
         def check(s,path):
-            nonlocal valid
+            nonlocal valid,greater
             print(s,path)
             if len(path) > 2:
                 for i in range(2,len(path)):
                     if path[i] != path[i-1] + path[i-2]:
+                        if path[i] > path[i-1] + path[i-2]:
+                            greater = True
                         return
                 if s == len(num):
                     valid = True
@@ -18,5 +22,10 @@ class Solution:
                 path.append(int(num[s:i+1]))
                 check(i+1,path)
                 path.pop()
+                if greater:
+                    greater = False
+                    return
+                if valid :
+                    return
         check(0,[])
         return valid
