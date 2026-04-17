ed5d863bbb198c4ef3e1c4ad45ac9886ed006e1d
Natnael Tigistu Gashaw <habtethiop@gmail.com>
2026-04-17 20:18:22 +0300

    Add solution for Design Browser History

--- leetcode/1472-design-browser-history/1472-design-browser-history.py
+++ leetcode/1472-design-browser-history/1472-design-browser-history.py
@@ -1,18 +1,34 @@
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.history = [homepage]
-        self.current = 0
-
+        self.forwards = []
+        self.f = 0
+        self.backward = [homepage]
+        self.b = 0
     def visit(self, url: str) -> None:
-        self.history = self.history[:self.current + 1]
-        self.history.append(url)
-        self.current = len(self.history) - 1
+        self.forwards.append(url)
+        self.f = 0
+        self.b += 1
+        self.backward.append(url)
 
     def back(self, steps: int) -> str:
-        self.current = max(0, self.current - steps)
-        return self.history[self.current]
-
+        while self.b > 0 and steps > 0:
+            self.b -= 1
+            self.f += 1
+            steps -= 1
+            self.forwards.append(self.backward.pop())        
+        return self.backward[-1]
     def forward(self, steps: int) -> str:
-        self.current = min(len(self.history) - 1, self.current + steps)
-        return self.history[self.current]
+        while self.f > 0 and steps > 0:
+            self.b += 1
+            self.f -= 1
+            steps -= 1
+            self.backward.append(self.forwards.pop())        
+        return self.backward[-1]
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
